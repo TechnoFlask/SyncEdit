f24c79507f6cbbffa5f10cdd55998515647f34f7
TechnoFlask <technoflask03@gmail.com>
2025-07-04 19:46:25 +0530

    fix: set topbar to sticky

--- src/features/editor/Editor.tsx
+++ src/features/editor/Editor.tsx
@@ -8,12 +8,12 @@ export function Editor() {
   const editor = useEditorConfig();
 
   return (
-    <div className="flex size-full flex-col gap-8 overflow-x-auto pb-20 print:overflow-visible print:p-0">
+    <div className="flex size-full flex-col gap-8 pb-20 print:p-0">
       <div className="sticky top-0 z-10 flex bg-gray-100 px-5 py-10 shadow-sm">
         <MenuBar />
       </div>
       <EditorContent
-        className="mx-auto min-h-screen w-5xl min-w-max px-8 print:w-full print:min-w-0"
+        className="mx-auto min-h-screen w-5xl min-w-max overflow-x-auto px-8 print:w-full print:min-w-0 print:overflow-visible"
         editor={editor}
       />
     </div>
