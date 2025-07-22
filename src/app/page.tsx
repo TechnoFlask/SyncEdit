7d653d4d2995aa13d2816fefcbb7545b3493be00
TechnoFlask <technoflask03@gmail.com>
2025-07-22 11:05:36 +0530

    feat: add suspense to recent gallery component for streaming

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,19 @@
 import { Navbar } from "@/features/home/navigation/Navbar";
 import { RecentDocuments } from "@/features/home/recents/RecentDocuments";
 import { TemplateGallery } from "@/features/home/templates/TemplateGallery";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <div className="flex h-screen w-screen flex-col bg-gray-200">
       <Navbar />
       <TemplateGallery />
-      <RecentDocuments />
+
+      <Suspense
+        fallback={<div className="w-full grow animate-pulse bg-white" />}
+      >
+        <RecentDocuments />
+      </Suspense>
     </div>
   );
 }
