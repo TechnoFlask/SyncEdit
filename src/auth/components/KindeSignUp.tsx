390e2d144e612285eacbbc96afa6020352b6fdeb
TechnoFlask <technoflask03@gmail.com>
2025-07-22 12:50:30 +0530

    fix: typo fix

--- src/auth/components/KindeSignUp.tsx
+++ src/auth/components/KindeSignUp.tsx
@@ -7,7 +7,7 @@ export function KindeSignUp() {
   return (
     <RegisterLink className="flex items-center gap-2">
       <IconUserPlus className="!size-6 !text-black transition-all duration-200" />
-      <button className="text-lg transition-all duration-200">Sign in</button>
+      <button className="text-lg transition-all duration-200">Sign up</button>
     </RegisterLink>
   );
 }
