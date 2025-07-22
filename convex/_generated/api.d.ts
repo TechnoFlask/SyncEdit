2955f9140f46c331c5c76ec334b466953156d933
TechnoFlask <technoflask03@gmail.com>
2025-07-22 17:37:18 +0530

    feat: add internalQuery for DRY document by id query

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -13,8 +13,10 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
+import type * as documents_internal_queries from "../documents/internal/queries.js";
 import type * as documents_mutations from "../documents/mutations.js";
 import type * as documents_queries from "../documents/queries.js";
+import type * as types from "../types.js";
 
 /**
  * A utility for referencing Convex functions in your app's API.
@@ -25,8 +27,10 @@ import type * as documents_queries from "../documents/queries.js";
  * ```
  */
 declare const fullApi: ApiFromModules<{
+  "documents/internal/queries": typeof documents_internal_queries;
   "documents/mutations": typeof documents_mutations;
   "documents/queries": typeof documents_queries;
+  types: typeof types;
 }>;
 export declare const api: FilterApi<
   typeof fullApi,
--- convex/documents/queries.ts
+++ convex/documents/queries.ts
@@ -1,4 +1,7 @@
+import { internal } from "@convex/_generated/api";
+import { Doc } from "@convex/_generated/dataModel";
 import { query } from "@convex/_generated/server";
+import { type Result } from "@convex/types";
 import { filter } from "convex-helpers/server/filter";
 import { paginationOptsValidator } from "convex/server";
 import { v } from "convex/values";
@@ -40,5 +43,24 @@ export const getAllDocuments = query({
       .withIndex("by_owner_id", (q) => q.eq("ownerId", userIdentity.subject))
       .order("desc")
       .paginate(paginationOpts);
+  },
+});
+
+export const getDocumentTitle = query({
+  args: { documentId: v.id("documents") },
+  async handler(
+    { runQuery },
+    { documentId },
+  ): Promise<Result<Doc<"documents">["title"], string>> {
+    const internalQueryResult = await runQuery(
+      internal.documents.internal.queries.queryDocumentById,
+      {
+        documentId,
+      },
+    );
+
+    if (!internalQueryResult.success) return internalQueryResult;
+
+    return { success: true, value: internalQueryResult.value.title };
   },
 });
