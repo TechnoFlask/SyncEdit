9010bdf5c6542f4990be2c97c5393e646495447c
TechnoFlask <technoflask03@gmail.com>
2025-07-22 12:50:55 +0530

    chore: add VERCEL_URL dependent env vars to support preview deployments

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,11 @@ const nextConfig: NextConfig = {
     ppr: true,
     useCache: true,
   },
+  env: {
+    KINDE_SITE_URL: `https://${process.env.VERCEL_URL}`,
+    KINDE_POST_LOGIN_REDIRECT_URL: `https://${process.env.VERCEL_URL}`,
+    KINDE_POST_LOGOUT_REDIRECT_URL: `https://${process.env.VERCEL_URL}`,
+  },
 };
 
 export default nextConfig;
