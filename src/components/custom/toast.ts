00f5ba10aa8255d5cba5d0af9ffa05e81da2585e
TechnoFlask <technoflask03@gmail.com>
2025-07-22 17:37:44 +0530

    feat: add custom warning variant

--- src/components/custom/toast.ts
+++ src/components/custom/toast.ts
@@ -3,7 +3,7 @@ import { ExternalToast, toast as sonnerToast } from "sonner";
 
 type titleT = (() => ReactNode) | ReactNode;
 
-const { success, error, ...otherVariants } = sonnerToast;
+const { success, error, warning, ...otherVariants } = sonnerToast;
 
 export const toast = {
   success(message: titleT, data?: ExternalToast) {
@@ -16,6 +16,13 @@ export const toast = {
     error(message, {
       ...data,
       className: "!bg-red-500 !text-white !text-xl !font-sans",
+    });
+  },
+
+  warning(message: titleT, data?: ExternalToast) {
+    warning(message, {
+      ...data,
+      className: "!bg-yellow-500 !text-black !text-xl !font-sans",
     });
   },
   ...otherVariants,
