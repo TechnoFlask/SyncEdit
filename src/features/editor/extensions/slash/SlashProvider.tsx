e342ac977c181ea05d6780fa2b48071da3cc8fc9
TechnoFlask <technoflask03@gmail.com>
2025-08-06 12:28:34 +0530

    fix: hide placeholder and commands list in printing

--- src/features/editor/extensions/slash/SlashProvider.tsx
+++ src/features/editor/extensions/slash/SlashProvider.tsx
@@ -16,7 +16,7 @@ export function SlashProvider({
       <SlashCmd.Root editor={editor}>
         <SlashCmd.Cmd>
           <SlashCmd.Empty>No commands available</SlashCmd.Empty>
-          <SlashCmd.List className="bg-white p-1 shadow-sm inset-shadow-sm [&>div]:flex [&>div]:flex-col">
+          <SlashCmd.List className="bg-white p-1 shadow-sm inset-shadow-sm print:hidden [&>div]:flex [&>div]:flex-col">
             {suggestions.map((item) => (
               <SlashCmd.Item
                 className="slashcmd flex cursor-pointer items-center gap-3 px-2 py-1 text-lg transition-colors duration-200 hover:bg-gray-100 data-[selected=true]:bg-gray-100"
